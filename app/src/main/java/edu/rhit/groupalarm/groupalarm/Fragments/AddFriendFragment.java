63043eb2ee655029a4b194cb821b9b6922710dd5
kevin1zc <kevinliuzc01@gmail.com>
2018-04-11 23:46:59 -0400

    my alarm fragment

--- app/src/main/java/edu/rhit/groupalarm/groupalarm/Fragments/AddFriendFragment.java
+++ app/src/main/java/edu/rhit/groupalarm/groupalarm/Fragments/AddFriendFragment.java
@@ -1,4 +1,4 @@
-package edu.rhit.groupalarm.groupalarm;
+package edu.rhit.groupalarm.groupalarm.Fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,16 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.rhit.groupalarm.groupalarm.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SettingsFragment.OnFragmentInteractionListener} interface
+ * {@link AddFriendFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SettingsFragment#newInstance} factory method to
+ * Use the {@link AddFriendFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragment extends Fragment {
+public class AddFriendFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +31,7 @@ public class SettingsFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public SettingsFragment() {
+    public AddFriendFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +41,11 @@ public class SettingsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
+     * @return A new instance of fragment AddFriendFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
+    public static AddFriendFragment newInstance(String param1, String param2) {
+        AddFriendFragment fragment = new AddFriendFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +66,7 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_add_friend, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
