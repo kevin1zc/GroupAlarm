df8596d82a84fb2ae96930051d7b6208ebea7d0e
kevin1zc <kevinliuzc01@gmail.com>
2018-04-14 13:30:31 -0400

    Added FriendsActivity

---
+++ app/src/main/java/edu/rhit/groupalarm/groupalarm/FriendsActivity.java
@@ -0,0 +1,28 @@
+package edu.rhit.groupalarm.groupalarm;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.rhit.groupalarm.groupalarm.Fragments.FriendListFragment;
+import edu.rhit.groupalarm.groupalarm.Fragments.FriendRequestFragment;
+
+public class FriendsActivity extends AppCompatActivity implements FriendListFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_friends);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.container_friends, new FriendListFragment());
+        ft.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container_friends, FriendRequestFragment.newInstance("", ""));
+        ft.addToBackStack("FriendList");
+        ft.commit();
+    }
+}
