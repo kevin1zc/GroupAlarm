34cc63ea6dbb0527fb4aa9f8ab5ad627a466c306
wykkevin <wykwykwykwykkevin@gmail.com>
2018-05-07 13:36:39 -0400

    Not much change.

--- app/src/main/java/edu/rhit/groupalarm/groupalarm/Adapters/SearchFriendAdapter.java
+++ app/src/main/java/edu/rhit/groupalarm/groupalarm/Adapters/SearchFriendAdapter.java
@@ -1,9 +1,12 @@
 package edu.rhit.groupalarm.groupalarm.Adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,23 +14,26 @@ import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 
+import edu.rhit.groupalarm.groupalarm.R;
 import edu.rhit.groupalarm.groupalarm.User;
 
 public class SearchFriendAdapter extends RecyclerView.Adapter<SearchFriendAdapter.SearchFriendViewHolder> {
     private ArrayList<User> searchedUsers;
     private DatabaseReference mUserRef;
     private Query mSearchRef;
+    private Context mContext;
 
-    public SearchFriendAdapter() {
+    public SearchFriendAdapter(Context context) {
         searchedUsers = new ArrayList<>();
         mUserRef = FirebaseDatabase.getInstance().getReference().child("users");
+        mContext = context;
     }
 
     @NonNull
     @Override
     public SearchFriendAdapter.SearchFriendViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_new_friend, parent, false);
+        return new SearchFriendAdapter.SearchFriendViewHolder(view);
     }
 
     @Override
@@ -37,7 +43,7 @@ public class SearchFriendAdapter extends RecyclerView.Adapter<SearchFriendAdapte
 
     @Override
     public int getItemCount() {
-        return 0;
+        return searchedUsers.size();
     }
 
     public class SearchFriendViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/edu/rhit/groupalarm/groupalarm/Fragments/FriendRequestFragment.java
+++ app/src/main/java/edu/rhit/groupalarm/groupalarm/Fragments/FriendRequestFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import edu.rhit.groupalarm.groupalarm.R;
 
@@ -16,34 +17,16 @@ import edu.rhit.groupalarm.groupalarm.R;
  * create an instance of this fragment.
  */
 public class FriendRequestFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText mInput;
 
 
     public FriendRequestFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FriendRequestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FriendRequestFragment newInstance(String param1, String param2) {
         FriendRequestFragment fragment = new FriendRequestFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +35,7 @@ public class FriendRequestFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
